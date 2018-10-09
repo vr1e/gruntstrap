bc79abc8c2a5aa368b20e3ce85a285f29b7e2ac3
Nikola Vrhovac <nikola.vrhovac@gowi.rs>
2018-10-09 14:39:50 +0200

    Track updating images inside src/img

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,12 @@
 'use-strict';
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 	require('jit-grunt')(grunt);
 	grunt.initConfig({
-	  	clean: ['dest/*'],
+		clean: {
+			dest: ['dest/**']},
 		watch: {
 			styles: {
-				files: ['src/styles/sass/*.scss', 'src/js/*.js', 'src/*.html'],
+				files: ['src/styles/sass/*.scss', 'src/js/*.js', 'src/*.html', 'src/img/**'],
 				tasks: ['sass', 'copy:track']
 			}
 		},
@@ -27,72 +28,79 @@ module.exports = function(grunt) {
 		copy: {
 			main: {
 				files: [{
-						expand: true,
-						cwd: 'bower_components/jquery/dist/',
-						src: 'jquery.min.js',
-						dest: 'dest/lib/js/',
-					}, {
-						expand: true,
-						cwd: 'bower_components/bootstrap-sass/assets/javascripts/',
-						src: 'bootstrap.min.js',
-						dest: 'dest/lib/js/',
-					}, {
-						expand: true,
-						cwd: 'bower_components/bootstrap-sass/assets/stylesheets/',
-						src: '**',
-						dest: 'src/styles/sass/bootstrap/',
-					}, {
-						expand: true,
-						cwd: 'bower_components/bootstrap-sass/assets/fonts/bootstrap/',
-						src: '**',
-						dest: 'dest/lib/fonts/',
-					}, {
-						expand: true,
-						cwd: 'bower_components/components-font-awesome/css',
-						src: 'font-awesome.min.css',
-						dest: 'dest/lib/css/',
-					}, {
-						expand: true,
-						cwd: 'bower_components/components-font-awesome/fonts',
-						src: '**',
-						dest: 'dest/lib/fonts/',
-					}, {
-						expand: true,
-						cwd: 'src/',
-						src: '*.html',
-						dest: 'dest/',
-					}, {
-						expand: true,
-						cwd: 'src/images/',
-						src: '**',
-						dest: 'dest/images/',
-					}, {
-						expand: true,
-						cwd: 'src/js/',
-						src: '*.js',
-						dest: 'dest/js/',
-					}, {
-						expand: true,
-						cwd: 'src/images/',
-						src: 'favicon.ico',
-						dest: 'dest/',
-					}
-				]
+					expand: true,
+					cwd: 'bower_components/jquery/dist/',
+					src: 'jquery.min.js',
+					dest: 'dest/lib/js/',
+				}, {
+					expand: true,
+					cwd: 'bower_components/bootstrap-sass/assets/javascripts/',
+					src: 'bootstrap.min.js',
+					dest: 'dest/lib/js/',
+				}, {
+					expand: true,
+					cwd: 'bower_components/bootstrap-sass/assets/stylesheets/',
+					src: '**',
+					dest: 'src/styles/sass/bootstrap/',
+				}, {
+					expand: true,
+					cwd: 'bower_components/bootstrap-sass/assets/fonts/bootstrap/',
+					src: '**',
+					dest: 'dest/lib/fonts/',
+				}, {
+					expand: true,
+					cwd: 'bower_components/components-font-awesome/css',
+					src: 'font-awesome.min.css',
+					dest: 'dest/lib/css/',
+				}, {
+					expand: true,
+					cwd: 'bower_components/components-font-awesome/fonts',
+					src: '**',
+					dest: 'dest/lib/fonts/',
+				}, {
+					expand: true,
+					cwd: 'src/js/lib/',
+					src: '*.js',
+					dest: 'dest/lib/js/',
+				}, {
+					expand: true,
+					cwd: 'src/',
+					src: '*.html',
+					dest: 'dest/',
+				}, {
+					expand: true,
+					cwd: 'src/img/',
+					src: '**',
+					dest: 'dest/img/',
+				}, {
+					expand: true,
+					cwd: 'src/js/',
+					src: '*.js',
+					dest: 'dest/js/',
+				}, {
+					expand: true,
+					cwd: 'src/img/',
+					src: 'favicon.ico',
+					dest: 'dest/',
+				}]
 			},
 			track: {
-				files: [
-					{
-						expand: true,
-						cwd: 'src/',
-						src: '*.html',
-						dest: 'dest/',
-					}, {
-						expand: true,
-						cwd: 'src/js/',
-						src: '*.js',
-						dest: 'dest/js/',
-					}
-				]
+				files: [{
+					expand: true,
+					cwd: 'src/',
+					src: '*.html',
+					dest: 'dest/',
+				}, {
+					expand: true,
+					cwd: 'src/js/',
+					src: '*.js',
+					dest: 'dest/js/',
+				}, {
+					expand: true,
+					cwd: 'src/img/',
+					src: '**',
+					dest: 'dest/img/',
+				}]
 			}
 		},
 		sass: {
@@ -107,10 +115,8 @@ module.exports = function(grunt) {
 			}
 		}
 	});
-	grunt.registerTask('default', ['clean', 'copy', 'sass', 'browserSync', 'watch']);
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.registerTask('default', ['clean:dest', 'copy:main', 'sass', 'browserSync', 'watch']);
 	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-browser-sync');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-clean');
